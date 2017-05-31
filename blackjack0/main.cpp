683663b5bf2a156ab966d5b6c64d18291e3543d3
Erik V. Ortega <erikvladimir@gmail.com>
2017-05-31 08:53:21 +0200

    Initial Commit

---
+++ blackjack0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  blackjack0
+//
+//  Created by Erik V. Ortega on 31/05/2017.
+//  Copyright © 2017 Erik V. Ortega. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
