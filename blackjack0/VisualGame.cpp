3b632c0148516b999a5c0e7fcd9e0ca18013cda9
Erik V. Ortega <erikvladimir@gmail.com>
2017-06-02 12:37:58 +0200

    add window border

--- blackjack0/VisualGame.cpp
+++ blackjack0/VisualGame.cpp
@@ -9,6 +9,18 @@
 
 #include "VisualGame.hpp"
 
+void rectangle(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2-x1);
+    mvhline(y2, x1, 0, x2-x1);
+    mvvline(y1, x1, 0, y2-y1);
+    mvvline(y1, x2, 0, y2-y1);
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
+}
+
 VisualGame::VisualGame():
     Game()
 {
@@ -52,6 +64,11 @@ void VisualGame::draw()
 {
     wclear(stdscr);
     
+    // draw borders
+    attron(COLOR_PAIR(0));
+    rectangle(0,0,25,80);
+    attron(COLOR_PAIR(0));
+    
     drawRoundMenu();
     drawScores();
     
@@ -60,7 +77,7 @@ void VisualGame::draw()
     
     if (m_status_msg.size() > 0)
     {
-        showMessageBox(1, 26, 4, 50, m_status_msg);
+        showMessageBox(1, 26, 4, 53, m_status_msg);
     }
     
     // put cursor out of sight
